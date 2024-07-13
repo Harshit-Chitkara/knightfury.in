a1ad8015dee6884b0431a6867b2cc30e97dddd39
Harshit Chitkara <harshit.chitkara@gmail.com>
2024-07-13 19:09:46 +0530

    docs: ✏️ speed-insights

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import Head from "next/head";
 import Masthead from "../components/masthead";
 import AboutUs from "../components/aboutus";
@@ -11,12 +11,12 @@ import Footer from "../components/footer";
 export default function Home() {
   return (
     <div>
-      <SpeedInsights />
       <Head>
         <title>Knight Fury | F1 in Schools</title>
         <meta name="description" content="" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <SpeedInsights />
       <Masthead />
       <AboutUs />
       <TeamHistory />
